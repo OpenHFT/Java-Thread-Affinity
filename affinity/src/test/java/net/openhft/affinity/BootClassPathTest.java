c08133e632c2e6799577c607ee6ef44e64f671b8
Peter Lawrey <peter.lawrey@chronicle.software>
2021-05-14 11:34:32 +0100

    Ignore a legacy way of finding a class, #71

--- affinity/src/test/java/net/openhft/affinity/BootClassPathTest.java
+++ affinity/src/test/java/net/openhft/affinity/BootClassPathTest.java
@@ -6,7 +6,9 @@ import static org.junit.Assert.assertTrue;
 
 public class BootClassPathTest {
     @Test
-    public void shouldDetectClassesOnClassPath() throws Exception {
+    public void shouldDetectClassesOnClassPath() {
+        if (!System.getProperty("java.version").startsWith("1.8"))
+            return;
         assertTrue(BootClassPath.INSTANCE.has("java.lang.Thread"));
         assertTrue(BootClassPath.INSTANCE.has("java.lang.Runtime"));
     }
