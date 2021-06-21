7a264a8e4022b9b482f7f5e0afffa38efa3df58b
Jerry Shea <jerry.shea@chronicle.software>
2021-06-21 14:46:16 +1000

    clean up and de-deprecate

--- affinity/src/test/java/net/openhft/affinity/impl/VanillaCpuLayoutTest.java
+++ affinity/src/test/java/net/openhft/affinity/impl/VanillaCpuLayoutTest.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author peter.lawrey
