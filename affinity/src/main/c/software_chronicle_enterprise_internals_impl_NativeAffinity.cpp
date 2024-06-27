f71c603d4814f96e071699c334e85b41d5ae3b1b
rogersimmons <roger.simmons@higherfrequencytrading.com>
2024-06-27 15:52:09 +0100

    Add windows support Tweak macOS support

--- affinity/src/main/c/software_chronicle_enterprise_internals_impl_NativeAffinity.cpp
+++ affinity/src/main/c/software_chronicle_enterprise_internals_impl_NativeAffinity.cpp
@@ -19,11 +19,16 @@
 #endif
 
 #include <jni.h>
-#include <sched.h>
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <string.h>
+#if defined(__linux__)
+  #include <sched.h>
+  #include <sys/syscall.h>
+  #include <sys/types.h>
+  #include <unistd.h>
+  #include <string.h>
+#elif defined(__APPLE__)
+  #include <mach/thread_policy.h>
+  #include <pthread.h>
+#endif
 #include <stdexcept>
 #include "software_chronicle_enterprise_internals_impl_NativeAffinity.h"
 
@@ -35,10 +40,7 @@
 JNIEXPORT jbyteArray JNICALL Java_software_chronicle_enterprise_internals_impl_NativeAffinity_getAffinity0
   (JNIEnv *env, jclass c) 
 {
-#ifndef __linux__
-    throw std::runtime_error("Not supported");
-#else
-
+#if defined(__linux__)
     // The default size of the structure supports 1024 CPUs, should be enough
     // for now In the future we can use dynamic sets, which can support more
     // CPUs, given OS can handle them as well
@@ -57,6 +59,23 @@ JNIEXPORT jbyteArray JNICALL Java_software_chronicle_enterprise_internals_impl_N
     env->SetByteArrayRegion(ret, 0, size, bytes);
 
     return ret;
+#elif defined(__APPLE__)
+    thread_port_t threadport = pthread_mach_thread_np(pthread_self());
+
+    struct thread_enterprise_internals_policy policy;
+    policy.affinity_tag = 0;
+    mach_msg_type_number_t count = THREAD_AFFINITY_POLICY_COUNT;
+    boolean_t get_default = FALSE;
+
+    if ((thread_policy_get(threadport,
+         THREAD_AFFINITY_POLICY, (thread_policy_t)&policy,
+         &count, &get_default)) != KERN_SUCCESS) {
+        return ~0LL;
+    }
+
+    return (jlong) policy.affinity_tag;
+#else
+    throw std::runtime_error("Not supported");
 #endif
 }
 
@@ -68,10 +87,7 @@ JNIEXPORT jbyteArray JNICALL Java_software_chronicle_enterprise_internals_impl_N
 JNIEXPORT void JNICALL Java_software_chronicle_enterprise_internals_impl_NativeAffinity_setAffinity0
   (JNIEnv *env, jclass c, jbyteArray affinity)
 {
-#ifndef __linux__
-    throw std::runtime_error("Not supported");
-#else
-    
+#if defined(__linux__)
     cpu_set_t mask;
     const size_t size = sizeof(mask);
     CPU_ZERO(&mask);
@@ -80,6 +96,23 @@ JNIEXPORT void JNICALL Java_software_chronicle_enterprise_internals_impl_NativeA
     memcpy(&mask, bytes, size);
 
     sched_setaffinity(0, size, &mask);
+#elif defined(__APPLE__)
+    thread_port_t threadport = pthread_mach_thread_np(pthread_self());
+
+    struct thread_enterprise_internals_policy policy;
+    policy.affinity_tag = affinity;
+
+    int rc = thread_policy_set(threadport,
+         THREAD_AFFINITY_POLICY, (thread_policy_t)&policy,
+         THREAD_AFFINITY_POLICY_COUNT);
+    if (rc != KERN_SUCCESS) {
+        jclass ex = (*env)->FindClass(env, "java/lang/RuntimeException");
+        char msg[100];
+        sprintf(msg, "Bad return value from thread_policy_set: %d", rc);
+        (*env)->ThrowNew(env, ex, msg);
+    }
+#else
+    throw std::runtime_error("Not supported");
 #endif
 }
 
