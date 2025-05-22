67fa0e0c659c8d103673a53f4adc56cbf67ef054
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-22 15:03:41 +0100

    Fix log message for unloaded JNI/JNA

--- affinity/src/main/java/net/openhft/affinity/impl/NullAffinity.java
+++ affinity/src/main/java/net/openhft/affinity/impl/NullAffinity.java
@@ -38,7 +38,7 @@ public enum NullAffinity implements IAffinity {
 
     @Override
     public void setAffinity(final BitSet affinity) {
-        LOGGER.trace("unable to set mask to {} as the JNIa nd JNA libraries and not loaded", Utilities.toHexString(affinity));
+        LOGGER.trace("unable to set mask to {} as the JNI and JNA libraries not loaded", Utilities.toHexString(affinity));
     }
 
     @Override
