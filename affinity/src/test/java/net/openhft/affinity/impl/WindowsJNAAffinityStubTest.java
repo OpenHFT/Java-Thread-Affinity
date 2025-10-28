6bfa3ea509c295043a86b86b4b634c019f509786
Peter Lawrey <peter.lawrey@chronicle.software>
2025-10-28 10:21:38 +0000

    Refactor code comments and formatting for clarity and consistency

--- affinity/src/test/java/net/openhft/affinity/impl/WindowsJNAAffinityStubTest.java
+++ affinity/src/test/java/net/openhft/affinity/impl/WindowsJNAAffinityStubTest.java
@@ -22,9 +22,7 @@ import org.junit.Test;
 import java.util.BitSet;
 import java.util.Locale;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class WindowsJNAAffinityStubTest {
 
