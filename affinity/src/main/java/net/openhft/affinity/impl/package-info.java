ae9e6f22a74183f642cdeb661c5745368fee2144
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-24 12:08:49 +0000

    Update copyright license in package-info.java to Apache-2.0

--- affinity/src/main/java/net/openhft/affinity/impl/package-info.java
+++ affinity/src/main/java/net/openhft/affinity/impl/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: LicenseRef-Proprietary
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
  */
 /**
  * Platform-specific implementations backing Chronicle thread affinity.
